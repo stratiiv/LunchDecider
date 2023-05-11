3c905edda904d331344bb0e497a850453ff4b319
stratiiv <stratiyvalter@gmail.com>
2023-05-11 14:26:46 +0300

    change menu and restaurant relation

--- lunch_decider/models.py
+++ lunch_decider/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 
 class Restaurant(models.Model):
@@ -12,17 +12,17 @@ class Restaurant(models.Model):
 
 
 class Menu(models.Model):
-    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    restaurant = models.OneToOneField(Restaurant, on_delete=models.CASCADE)
     date = models.DateField()
     items = models.TextField()
 
     def __str__(self):
-        return str(self.date)
+        return f"Menu for {self.restaurant.name} on {self.date}"
 
 
 class Vote(models.Model):
     menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
-    employee = models.ForeignKey(User, on_delete=models.CASCADE)
+    employee = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_at = models.DateField(auto_now_add=True)
 
     def __str__(self):
